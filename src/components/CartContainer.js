0e613ee520ac82780d5a07ce8ad673b232b8e6b6
platisam <platisam@gmail.com>
2023-08-25 14:00:23 +0200

    displaying items in CartContainer  using useSelector hook

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -15,6 +15,26 @@ const CartContainer = () => {
     );
   }
 
-  return <div>CartContainer</div>;
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn">clear cart</button>
+      </footer>
+    </section>
+  );
 };
 export default CartContainer;
