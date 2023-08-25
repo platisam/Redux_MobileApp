049d43111f5e7175606ac3d39375a736404c3425
platisam <platisam@gmail.com>
2023-08-25 12:56:34 +0200

    creating CartContainer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 };
---
+++ src/components/CartContainer.js
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
+
+const CartContainer = () => {
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return <div>CartContainer</div>;
+};
+export default CartContainer;
