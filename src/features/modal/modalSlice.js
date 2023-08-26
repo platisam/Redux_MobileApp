482d4c3efe8eddb288c34e645a29bcd0c17be911
platisam <platisam@gmail.com>
2023-08-26 17:41:45 +0200

    creating openModal and closeModal actions in modalSlice reducer

--- src/features/modal/modalSlice.js
+++ src/features/modal/modalSlice.js
@@ -8,8 +8,15 @@ const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    
-    
+    openModal: (state, action) => {
+      state.isOpen = true;
+    },
+    closeModal: (state, action) => {
+      state.isOpen = false;
     },
   },
 });
+
+export const { openModal, closeModal } = modalSlice.actions;
+
+export default modalSlice.reducer;
