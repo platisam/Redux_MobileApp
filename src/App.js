9c6f2a606fc2ed2172d9613c5b683cb6fcfd6fcc
platisam <platisam@gmail.com>
2023-08-26 16:17:41 +0200

    setting calculateTotals action in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 const App = () => {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
